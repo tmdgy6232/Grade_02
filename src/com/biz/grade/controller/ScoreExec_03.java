3ef6783b08a5d829487c890171b902f45ea3d624
tmdgy6232 <tmdgy6232@naver.com>
2019-06-14 16:45:11 +0900

    grade

---
+++ src/com/biz/grade/controller/ScoreExec_03.java
@@ -0,0 +1,37 @@
+package com.biz.grade.controller;
+
+import java.io.FileNotFoundException;
+
+import com.biz.grade.service.ScoreService;
+import com.biz.grade.service.ScoreServiceImp_05;
+
+public class ScoreExec_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String scoreFile = "src/com/biz/grade/data/score_02.txt";
+
+		/*
+		 * try문으로 묶여야되는 명령문이 있을 경우
+		 * try문 안에서 객체나 변수를 생성하면
+		 * try가 문 안에 모든 코드를 집중시켜야 한다.
+		 * 
+		 * 그래서 선언문과 생성문을 별도로 분리하는것이 좋다.
+		 */
+		ScoreService sService = null;//선언문
+		try {
+			sService = new ScoreServiceImp_05(scoreFile);//생성문
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sService.inputScore(0);
+		sService.total();
+		sService.rank();
+		
+		sService.viewList();
+	
+		
+	}
+
+}
